0811146c5be758eff74a0f81728f5de793397903
Eric <eric.robert@simpleclinic.net>
2023-06-04 06:58:49 +1000

    Gets the value from the textview

--- win.c
+++ win.c
@@ -9,6 +9,7 @@ struct _TokenizerAppWindow
     GtkWidget *menu;
     GtkButton *encode_button;
     GtkButton *decode_button;
+    GtkTextView *text_view;
 };
 
 G_DEFINE_TYPE(TokenizerAppWindow, tokenizer_app_window, GTK_TYPE_APPLICATION_WINDOW);
@@ -23,6 +24,26 @@ static void
 decode_button_clicked(GtkButton *button,gpointer user_data)
 {
     g_print("Button clicked\n");
+    // Cast the user_data back to a GtkTextView pointer
+    TokenizerAppWindow *win = TOKENIZER_APP_WINDOW(user_data);
+    GtkTextView *text_view = win->text_view;
+
+    // Get the text buffer associated with the text view
+    GtkTextBuffer *buffer = gtk_text_view_get_buffer(text_view);
+
+    // Get the start and end iterator of the text buffer
+    GtkTextIter start, end;
+    gtk_text_buffer_get_start_iter(buffer, &start);
+    gtk_text_buffer_get_end_iter(buffer, &end);
+
+    // Extract the text from the buffer
+    gchar *text = gtk_text_buffer_get_text(buffer, &start, &end, FALSE);
+
+    // Do something with the text
+    g_print("Text value: %s\n", text);
+
+    // Free the allocated text
+    g_free(text);
 }
 
 static void
@@ -38,7 +59,8 @@ tokenizer_app_window_init (TokenizerAppWindow *win)
     g_object_unref (builder);
 
     g_signal_connect(win->encode_button,"clicked",G_CALLBACK(encode_button_clicked),NULL);
-    g_signal_connect(win->decode_button,"clicked",G_CALLBACK(decode_button_clicked),NULL);
+    g_signal_connect(win->decode_button,"clicked",G_CALLBACK(decode_button_clicked),win);
+
 }
 
 static void
@@ -48,6 +70,7 @@ tokenizer_app_window_class_init (TokenizerAppWindowClass *class)
     gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (class), TokenizerAppWindow , menu);
     gtk_widget_class_bind_template_child(GTK_WIDGET_CLASS(class),TokenizerAppWindow,encode_button);
     gtk_widget_class_bind_template_child(GTK_WIDGET_CLASS(class),TokenizerAppWindow,decode_button);
+    gtk_widget_class_bind_template_child(GTK_WIDGET_CLASS(class),TokenizerAppWindow,text_view);
 }
 
 TokenizerAppWindow *
