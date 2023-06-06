234d04da67d639f3d8ee868de9d33c8fe6451ae9
Eric <eric.robert@simpleclinic.net>
2023-06-07 05:44:29 +1000

    Checks if the jwt payload needs to be padded before passing to g_base64_decode Issue #1

--- jwt.c
+++ jwt.c
@@ -3,26 +3,23 @@
 
 
 char *jwt_decode(char *jwt) {
-    /**
-     * No, the number of padding characters (equals signs) in Base64 encoding depends on the length of the input. The padding characters are used to ensure that the length of the encoded string is a multiple of 4.
-
-In Base64 encoding, the padding characters can be one or two equals signs (=). The rules for adding padding characters are as follows:
-
-If the input length is not a multiple of 3, padding is required.
-If the input length is one character more than a multiple of 3, one equals sign is added as padding.
-If the input length is two characters more than a multiple of 3, two equals signs are added as padding.
-For example:
-
-If the input length is 4, no padding is needed.
-If the input length is 5, one equals sign is added as padding.
-If the input length is 6, no padding is needed.
-If the input length is 7, two equals signs are added as padding.
-The equals signs in Base64 encoding do not represent any specific value. They are used solely for padding purposes.
-     */
     gchar** data = g_strsplit(jwt, ".", 3);
+
+    char *padded = data[1];
+    int len = strlen(padded);
+    printf("Len: %d\n", len);
+    printf("Mod4 = %d\n", len%4);
+    if (len%4 == 2) {
+        padded = g_strconcat(padded, "==", NULL);
+    } else if (len%4 == 1) {
+        padded = g_strconcat(padded, "=", NULL);
+    }
+
+    printf("Padded: %s\n", padded);
+
     guchar *result;
     gsize result_len;
-    result = g_base64_decode(data[1], &result_len);
+    result = g_base64_decode(padded, &result_len);
     printf("Result: ");
     for (gsize i = 0; i < result_len; i++) {
         printf("%02X ", result[i]);
