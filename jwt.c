3ab8763b0f3b683ac50a544ecfef39b84b65bc72
Eric <eric.robert@simpleclinic.net>
2023-06-08 08:36:15 +1000

    Removes the print

--- jwt.c
+++ jwt.c
@@ -4,7 +4,6 @@
 
 char *jwt_decode(char *jwt) {
     gchar** data = g_strsplit(jwt, ".", 3);
-
     char *padded = data[1];
     int len = strlen(padded);
     printf("Len: %d\n", len);
@@ -14,16 +13,11 @@ char *jwt_decode(char *jwt) {
     } else if (len%4 == 1) {
         padded = g_strconcat(padded, "=", NULL);
     }
-
     printf("Padded: %s\n", padded);
-
     guchar *result;
     gsize result_len;
     result = g_base64_decode(padded, &result_len);
     printf("Result: ");
-    for (gsize i = 0; i < result_len; i++) {
-        printf("%02X ", result[i]);
-    }
     printf("\n");
     return result;
 }
